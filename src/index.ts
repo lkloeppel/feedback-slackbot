7898a2450a9c43d2fd947008358266b3c1d81806
Lukas Kloeppel <lkloeppel@seek.com.au>
2021-04-11 11:44:19 +1000

    Smaller fixes

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,23 @@ import getFeedbackCommand from './handlers/getFeedback';
 import getFeedbackForUserAction from './handlers/getFeedbackForUser';
 import chatMessageHandler from './handlers/chatMessage';
 
-const bot = new App({
+const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN
+  token: process.env.SLACK_app_TOKEN
 });
 
-bot.message(chatMessageHandler);
-bot.command('/start-feedback-reminder', startFeedbackCommand);
-bot.command('/stop-feedback-reminder', stopFeedbackCommand);
-bot.command('/get-feedback', getFeedbackCommand);
-bot.action('get-feedback-for-user', getFeedbackForUserAction);
+app.message(chatMessageHandler);
+app.command('/start-feedback-reminder', startFeedbackCommand);
+app.command('/stop-feedback-reminder', stopFeedbackCommand);
+app.command('/get-feedback', getFeedbackCommand);
+app.action('get-feedback-for-user', getFeedbackForUserAction);
 
-// cron.schedule('*/10 * * * *', () => feedbackReminderAction(bot));
+// cron.schedule('*/10 * * * *', () => feedbackReminderAction(app));
 
 (async () => {
+  const port = Number(process.env.PORT) || 3000;
+
   // Start your app
-  await bot.start(Number(process.env.PORT) || 3000);
+  await app.start(port);
+  console.log(`Feedback Bot is running on ${port}!`);
 })();
